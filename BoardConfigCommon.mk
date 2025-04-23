da02aae7ad69e893b732c51e036247b1b84de5a9
Jamie Falconer <mrfluffyoven@gmail.com>
2025-04-23 17:14:38 +0100

    use vendor_dlkm modules

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -169,7 +169,8 @@ TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone26/temp"
 
 # Modules 
 TW_LOAD_VENDOR_BOOT_MODULES := true
-TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules) $(shell ls $(DEVICE_PATH)/recovery/root/vendor/lib/modules)\")
+#TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules) $(shell ls $(DEVICE_PATH)/recovery/root/vendor/lib/modules)\")
+TW_LOAD_VENDOR_DLKM_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules) $(shell ls $(DEVICE_PATH)/recovery/root/vendor/lib/modules)\") 
 
 # Logging
 TARGET_USES_LOGD := true
